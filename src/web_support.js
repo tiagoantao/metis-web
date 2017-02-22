6e6bc6feee0044193882e3966aea9d511175772d
Tiago Antao <tiagoantao@gmail.com>
2017-02-22 14:03:42 -0700

    bringing metis as a rollup dependency

---
+++ src/web_support.js
@@ -0,0 +1,6 @@
+import Rx from 'rxjs/Rx'
+//import {run} from '@cycle/rxjs-run'
+//import {makeDOMDriver} from '@cycle/dom'
+
+//const {makeDOMDriver} = DOM  // rollup/cycles boondoggle
+//export {run}
