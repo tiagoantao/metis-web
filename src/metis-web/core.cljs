3a5634beb74c1eb556574e3f770496f8fca247b0
Tiago Antao <tiagoantao@gmail.com>
2017-09-29 14:34:14 -0600

    vega operational

--- src/metis-web/core.cljs
+++ src/metis-web/core.cljs
@@ -1,56 +1,41 @@
 (ns metis-web.core
   "A web interface to Metis, the JavaScript population genetics simulator"
   {:author "Tiago Antao"}
-  (:require [cljsjs.vega :as vg]
-            [cljsjs.vega-lite :as vl]
+  (:require [cljsjs.vega]
+            [cljsjs.vega-lite]
             [cljsjs.d3]
             ))
 
-(comment
-(vl/unique [1 2 3] identity)
-(vg/parse)
-vl
-ve
-(enable-console-print!)
-)
+(def spec "
+{
+  \"$schema\": \"https://vega.github.io/schema/vega-lite/v2.json\",
+  \"description\": \"A simple bar chart with embedded data.\",
+  \"data\": {
+    \"values\": [
+      {\"a\": \"A\",\"b\": 28}, {\"a\": \"B\",\"b\": 55}, {\"a\": \"C\",\"b\": 43},
+      {\"a\": \"D\",\"b\": 91}, {\"a\": \"E\",\"b\": 81}, {\"a\": \"F\",\"b\": 53},
+      {\"a\": \"G\",\"b\": 19}, {\"a\": \"H\",\"b\": 87}, {\"a\": \"I\",\"b\": 52}
+    ]
+  },
+  \"mark\": \"bar\",
+  \"encoding\": {
+    \"x\": {\"field\": \"a\", \"type\": \"ordinal\"},
+    \"y\": {\"field\": \"b\", \"type\": \"quantitative\"}
+  }
+}
 
-(comment
+")
+
+
 (defn ^:export run
   [canvas-name]
-    (set! (.-onload js/window)
-        (let [canvas (.getElementById js/document canvas-name)
-              width (.-width canvas)
-              height (.-height canvas)
-              ctx (.getContext canvas"2d")]
-          (.beginPath ctx)
-          (.arc ctx 95 50 40 0 6.28)
-          (.stroke ctx)
-          ))
-
-  (prn 2))
-)
-
-(def spec
-"
-\"$schema\": \"https://vega.github.io/schema/vega-lite/v2.0.json\",
-\"description\": \"A simple bar chart with embedded data.\",
-\"data\": {
-         \"values\": [
-                    {\"a\": \"A\",\"b\": 28}, {\"a\": \"B\",\"b\": 55}, {\"a\": \"C\",\"b\": 43},
-                    {\"a\": \"D\",\"b\": 91}, {\"a\": \"E\",\"b\": 81}, {\"a\": \"F\",\"b\": 53},
-                    {\"a\": \"G\",\"b\": 19}, {\"a\": \"H\",\"b\": 87}, {\"a\": \"I\",\"b\": 52}
-                    ]
-         },
-\"mark\": \"bar\",
-\"encoding\": {
-             \"x\": {\"field\": \"a\", \"type\": \"ordinal\"},
-             \"y\": {\"field\": \"b\", \"type\": \"quantitative\"}
-             }
-  }
-"
-)
-
-(defn ^:export run
-  [div-name]
-  ;(.embed div-name spec)
-  )
+  (set! (.-onload js/window)
+        (let [jspec (.parse js/JSON spec)]
+          
+          (set! (.-width jspec) 600)
+          (set! (.-heigth jspec) 600)
+          (let [cspec (.-spec (.compile js/vl jspec))
+                view (new js/vega.View (.parse js/vega cspec))
+                renderer (.renderer view "canvas")]
+            (.run (.initialize renderer "#vis"))
+            ))))
