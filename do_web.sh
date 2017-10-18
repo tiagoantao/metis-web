d4fc5b7200e3bee6c4f3c0b94f682c306b3e6d31
Tiago Antao <tiagoantao@gmail.com>
2017-10-17 19:04:56 -0600

    loading metis on the web

--- do_web.sh
+++ do_web.sh
@@ -1,3 +1,4 @@
+npm install
 cd static/
 node build.js
 cd ..
@@ -5,3 +6,6 @@ cd ..
 cp static/out/index.html resources/public/
 mkdir -p resources/public/js/jquery-knob/dist
 cp node_modules/jquery-knob/dist/jquery.knob.min.js resources/public/js/jquery-knob/dist
+
+mkdir -p resources/public/js/metis
+cp "node_modules/@tiagoantao/metis/lib/metis/all.js" resources/public/js/metis
