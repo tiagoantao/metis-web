ad235c81a473a996c8c0ae309c47635d93215299
Tiago Antao <tiagoantao@gmail.com>
2017-10-17 18:49:17 -0600

    ad-hoc static site build

--- do_web.sh
+++ do_web.sh
@@ -1,2 +1,7 @@
 cd static/
 node build.js
+cd ..
+
+cp static/out/index.html resources/public/
+mkdir -p resources/public/js/jquery-knob/dist
+cp node_modules/jquery-knob/dist/jquery.knob.min.js resources/public/js/jquery-knob/dist
