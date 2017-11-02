40338d2876657d3bc06bd94ac8f59652f7d96f39
Tiago Antao <tiagoantao@gmail.com>
2017-11-01 23:32:54 -0600

    cycles and RxJs usage

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,35 @@
 import Rx from 'rxjs/Rx'
 
-export function App (sources) {
-  const vtree$ = Rx.Observable.of(
-    <div>My Awesome Cycle.js app</div>
-  )
+export const App = (sources) => {
+  const num_gens$ = sources.DOM.select('#num_gens')
+    .events('change')
+    .map(ev => parseInt(ev.target.value))
+    .startWith(50)
+
+  const num_gens = 
+
+  const simulate$ = sources.DOM.select('#simulate')
+    .events('click')
+    .map(ev => parseInt(ev.target.value))
+    .startWith(0)
+
+  const mrg = Rx.Observable.zip(num_gens$, simulate$)
+    .subscribe(x => console.log(x))
+
+  const vdom$ = simulate$.map(num_gens =>
+    <div>
+      <div>
+        <input type="number" id="num_gens" value="{num_gens}"/>
+        <button id="simulate" value="1">Simulate</button>
+      </div>
+      <div>bla {String(num_gens)}
+      </div>
+    </div>      
+      )
+
   const sinks = {
-    DOM: vtree$
+    DOM: vdom$
   }
+  
   return sinks
 }
