7e0142af3895976ef5e4bae0a841f13654c48a60
Tiago Antao <tiagoantao@gmail.com>
2017-11-05 21:12:15 -0700

    driver: ongoing

--- src/driver.js
+++ src/driver.js
@@ -2,35 +2,36 @@ import xs from 'xstream'
 import {adapt} from '@cycle/run/lib/adapt'
 import {sim_cycle} from '@tiagoantao/metis'
 
-const stack = []
-stack.listeners = []
-
-stack.push = (elem) => {
-    for (listener of listeners) {
-	listener.call(event)
-    }
-}
-
-const add_event (arr, e) => {
+const add_listener (arr, l) => {
     stack.push = (elem) => {
-	Array.prototype.push.call(stack, e)
+        Array.prototype.push.call(stack, l)
     }
 }
 
-export const makeMetisDriver = (start_state$) => {
-    start_state$.addListener({
-	next: state => {stack.push(state)},
-	error: () => {},
-        complete: () => {}
-    })
+export const makeMetisDriver = () => {
+    const stack = []
+    stack.listeners = []
+    stack.push = (elem) => {
+        for (listener of listeners) {
+            listener.call(event)
+        }
+    }
+     
+    const metis_driver (in_state$) {
+        in_state$.addListener({
+            next: state => {stack.push(state)},
+            error: () => {},
+            complete: () => {}
+        })
 
-    const sim_state$ = xs.create({
-	start: listener => {
-	    stack.add_event(listner)
-	    listener.next(new_state)
-	},
-	stop: () => {}
-    })
+        const sim_state$ = xs.create({
+            start: listener => {
+            },
+            stop: () => {}
+        })
 
-    return adapt(sim_state$)
+        return adapt(sim_state$)
+    }
+
+    return metis_driver
 }
