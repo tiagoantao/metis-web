d0aaabae7af759014379f20bf2b663ccbd1bb8f7
Tiago Antao <tiagoantao@gmail.com>
2018-01-24 17:22:01 -0700

    Stepping-Stone: Begin

--- src/app.jsx
+++ src/app.jsx
@@ -10,6 +10,7 @@ import {StochasticityApp} from './stochasticity'
 import {DeclineApp} from './decline'
 import {SelectionAppFactory} from './selection'
 import {IslandApp} from './island'
+import {SteppingStoneApp} from './stepping-stone'
 import {SexRatioApp} from './sex-ratio'
 import {AlphaApp} from './alpha'
 import {SelectionDriftApp} from './selection-drift'
@@ -50,8 +51,7 @@ const uikit_template =
             <div className="uk-navbar-dropdown">
               <ul className="uk-nav uk-navbar-dropdown-nav">
                 <li><a href="#" id="menu-island">Island</a></li>
-                <li><a href="#">Stepping-stone 1D</a></li>
-                <li><a href="#">Stepping-stone 2D</a></li>
+                <li><a href="#" id="menu-stoned">Stepping-stone</a></li>
               </ul>
             </div>
           </li>
@@ -114,6 +114,10 @@ export const App = (sources) => {
   const island_menu$ = sources.DOM.select('#menu-island').events('click')
                               .startWith({timeStamp: -1,
                                           srcElement: {id: 'menu-island'}})
+
+  const stoned_menu$ = sources.DOM.select('#menu-stoned').events('click')
+                              .startWith({timeStamp: -1,
+                                          srcElement: {id: 'menu-stoned'}})
   
   const sex_ratio_menu$ = sources.DOM.select('#menu-sex-ratio').events('click')
                                  .startWith({timeStamp: -1,
@@ -132,7 +136,7 @@ export const App = (sources) => {
     single_menu$, wf_menu$, freq_menu$, stoch_menu$,
     decline_menu$,
     dominant_menu$, recessive_menu$, hz_menu$,
-    island_menu$,
+    island_menu$, stoned_menu$,
     sex_ratio_menu$, alpha_menu$,
     sel_drift_menu$)
                           .map(entries => {
@@ -171,7 +175,10 @@ export const App = (sources) => {
 
   const island_pop = IslandApp({DOM: sources.DOM, metis: sources.metis})
   const il_dom$ = island_pop.DOM
-  
+
+  const stoned_pop = SteppingStoneApp({DOM: sources.DOM, metis: sources.metis})
+  const sst_dom$ = stoned_pop.DOM
+
   const sex_ratio_pop = SexRatioApp({DOM: sources.DOM, metis: sources.metis})
   const sr_dom$ = sex_ratio_pop.DOM
 
@@ -187,7 +194,7 @@ export const App = (sources) => {
 			      sp_dom$, wf_dom$, fq_dom$, sch_dom$,
                               dc_dom$,
                               dom_dom$, rec_dom$, hz_dom$,
-			      il_dom$,
+			      il_dom$, sst_dom$,
                               sr_dom$, ap_dom$,
                               sd_dom$)
     .map(arr =>
@@ -220,6 +227,9 @@ export const App = (sources) => {
         <div style={arr[0] === 'menu-island' ? 'display: block' : 'display: none'}>
           {arr[9]}
         </div>
+        <div style={arr[0] === 'menu-stoned' ? 'display: block' : 'display: none'}>
+          {arr[9]}
+        </div>
         <div style={arr[0] === 'menu-sex-ratio' ? 'display: block' : 'display: none'}>
           {arr[10]}
         </div>
@@ -238,7 +248,7 @@ export const App = (sources) => {
       wf_pop.metis, single_pop.metis, freq_pop.metis, stoch_pop.metis,
       decline_pop.metis, 
       dominant_pop.metis, recessive_pop.metis, hz_pop.metis,
-      island_pop.metis,
+      island_pop.metis, stoned_pop.metis,
       sex_ratio_pop.metis, alpha_pop.metis,
       sel_drift_pop.metis)
     
---
+++ src/stepping-stone.jsx
@@ -0,0 +1,170 @@
+import Rx from 'rxjs/Rx'
+
+import {Plot} from './plot.js'
+import {Selector} from './selector.js'
+import {Slider} from './slider.js'
+
+
+import {
+  gn_generate_unlinked_genome,
+  gn_MicroSatellite,
+  gn_SNP,
+  i_assign_random_sex,
+  integrated_create_randomized_genome,
+  integrated_generate_individual_with_genome,
+  ops_culling_KillOlderGenerations,
+  ops_p_MigrationSteppingStoneFixed,
+  ops_rep_StructuredSexualReproduction,
+  ops_stats_demo_SexStatistics,
+  ops_stats_hz_ExpHe,
+  ops_stats_hz_ExpHeDeme,
+  ops_stats_NumAl,
+  ops_wrap_list,
+  p_assign_fixed_size_population,
+  p_generate_n_inds,
+  sp_Species} from '@tiagoantao/metis-sim'
+
+
+const prepare_sim_state = (tag, d1, d2, deme_size, num_migs,
+                           num_markers, marker_type) => {
+  const genome_size = num_markers
+
+  const unlinked_genome = gn_generate_unlinked_genome(
+    genome_size, () => {
+      return marker_type === 'SNP'?
+        new gn_SNP() :
+        new gn_MicroSatellite(Array.from(new Array(10), (x,i) => i))
+      })
+  const species = new sp_Species('unlinked', unlinked_genome)
+  const operators = ops_wrap_list([
+    new ops_rep_StructuredSexualReproduction(species, deme_size, num_demes),
+    new ops_culling_KillOlderGenerations(),
+    new ops_p_MigrationIslandFixed(num_migs),
+    new ops_stats_demo_SexStatistics(),
+    new ops_stats_hz_ExpHe(),
+    new ops_stats_hz_ExpHeDeme()
+  ])
+  const individuals = p_generate_n_inds(deme_size * num_demes, () =>
+    i_assign_random_sex(integrated_generate_individual_with_genome(
+      species, 0, integrated_create_randomized_genome)))
+  p_assign_fixed_size_population(individuals, num_demes)
+  const state = {
+    global_parameters: {tag, stop: false},
+    individuals, operators, cycle: 1}
+  return state
+}
+
+
+export const SteppingStoneApp = (sources) => {
+
+  const tag = 'stepping-stone'
+
+  const my_metis$ = sources.metis.filter(
+    state => state.global_parameters.tag === tag)
+
+  const exphe$ = my_metis$.map(state => {
+    var cnt = 1
+    return state.global_parameters.ExpHe.unlinked.map(exphe => {
+      return {
+        x: state.cycle - 1, y: exphe, marker: 'M' + cnt++}})
+  })
+
+  const dexphe$ = my_metis$.map(state => {
+    var cnt = 1
+      console.log(state.global_parameters)
+    return state.global_parameters.DemeExpHe[0].unlinked.map(exphe => {
+      return {
+        x: state.cycle - 1, y: exphe, marker: 'M' + cnt++}})
+  })
+    
+  const marker_type_c = Selector({DOM: sources.DOM},
+                                 {className: '.' + tag + '-marker_type',
+                                  label: 'marker type:'})
+  let marker_type
+  marker_type_c.value.subscribe(v => marker_type = v)
+  
+  const deme_size_c = Slider({DOM: sources.DOM},
+                             {className: '.' + tag + '-deme_size', label: 'deme size:',
+                              step: 10, min: 10, value: 50, max: 100})
+  let deme_size
+  deme_size_c.value.subscribe(v => deme_size = v)
+
+
+  const num_demes_c = Slider({DOM: sources.DOM},
+                             {className: '.' + tag + '-num_demes', label: 'num demes:',
+                              step: 1, min: 2, value: 2, max: 10})
+  let num_demes
+  num_demes_c.value.subscribe(v => num_demes = v)
+
+  const num_migs_c = Slider({DOM: sources.DOM},
+                            {className: '.' + tag + '-num_migs', label: 'num migrants:',
+                             step: 1, min: 0, value: 1, max: 9})
+  let num_migs
+  num_migs_c.value.subscribe(v => num_migs = v)
+  
+  
+  const num_cycles_c = Slider({DOM: sources.DOM},
+                              {className: '.' + tag + '-num_cycles', label: 'cycles:',
+                               step: 10, min: 10, value: 20, max: 200})
+  let num_cycles
+  num_cycles_c.value.subscribe(v => num_cycles = v)
+
+  const num_markers_c = Slider({DOM: sources.DOM},
+                               {className: '.' + tag + '-num_markers', label: 'markers:',
+                                step: 1, min: 1, value: 4, max: 20})
+  let num_markers
+  num_markers_c.value.subscribe(v => num_markers = v)
+
+  const exphe_plot = Plot(
+    {id: tag + '-exphe', y_label: 'Expected Hz - Meta population'},
+    {DOM: sources.DOM, vals: exphe$})
+
+  const dexphe_plot = Plot(
+    {id: tag + '-dexphe', y_label: 'Expected Hz - A Deme'},
+    {DOM: sources.DOM, vals: dexphe$})
+
+  const simulate$ = sources.DOM.select('#' + tag)
+                           .events('click')
+                           .map(ev => parseInt(ev.target.value))
+  
+  const metis$ = simulate$.map(_ => {
+    return Rx.Observable.from([
+      {num_cycles, state: prepare_sim_state(tag,
+                                            num_demes, deme_size, num_migs,
+                                            num_markers, marker_type)}
+    ])
+  })
+
+  const vdom$ = Rx.Observable
+                  .combineLatest(
+                    marker_type_c.DOM,
+                    deme_size_c.DOM, num_demes_c.DOM, num_migs_c.DOM,
+                    num_cycles_c.DOM, num_markers_c.DOM,
+                    exphe_plot.DOM, dexphe_plot.DOM)
+                  .map(([marker_type,
+                         num_demes, deme_size, num_migs,
+                         num_cycles, num_markers,
+                         exphe, dexphe]) =>
+                           <div>
+                             <div>
+                               {marker_type}
+                               {num_demes}
+                               {num_migs}
+                               {deme_size}
+                               {num_cycles}
+                               {num_markers}
+                               <br/>
+                               <button id={tag} value="1">Simulate</button>
+                             </div>
+                             {exphe}
+                             {dexphe}
+                           </div>
+                  )
+
+  const sinks = {
+    DOM: vdom$,
+    metis: metis$
+  }
+  
+  return sinks
+}
