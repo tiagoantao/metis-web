2f61ced76f74c62b86e4bc2907374594c0fed55d
Tiago Antao <tiagoantao@gmail.com>
2018-01-30 12:37:43 -0700

    less ugly UI

--- src/selector.js
+++ src/selector.js
@@ -2,35 +2,37 @@ import {div, option, p, select, span} from '@cycle/dom'
 
 
 export const Selector = (sources, props) => {
-  const DOM = sources.DOM.select(props.className)
+    const DOM = sources.DOM.select(props.className)
 
-  const new_value$ = DOM
-    .select('select')
-    .events('input')
-    .map(ev => ev.target.value)
-    .startWith("SNP")
+    const new_value$ = DOM
+          .select('select')
+          .events('input')
+          .map(ev => ev.target.value)
+          .startWith("SNP")
 
-  const __conv_opts = (opts) => {
-    return {selected: "SNP"}
-  }
+    const __conv_opts = (opts) => {
+        return {selected: "SNP"}
+    }
 
-  const opts = __conv_opts(props.opts)
-  
-  const vdom$ = new_value$
-    .map(state =>
-      div(props.className, [
-	span('.label', [props.label]),
-	select({attrs: {name}}, [
-	  option('Microsat'),
-	  option({attrs: {selected: 1}}, 'SNP')
-	])
-      ])
-    )
+    const opts = __conv_opts(props.opts)
+    
+    const vdom$ = new_value$
+          .map(state =>
+               div({attrs: {
+                   style: 'text-align: center',
+                   className: props.className}}, [
+                       span('.label', [props.label + ' ']),
+                       select({attrs: {name}}, [
+                           option('Microsat'),
+                           option({attrs: {selected: 1}}, 'SNP')
+                       ])
+                   ])
+              )
 
-  const sinks = {
-    DOM: vdom$,
-    value: new_value$
-  }
+    const sinks = {
+        DOM: vdom$,
+        value: new_value$
+    }
 
-  return sinks
+    return sinks
 }
--- src/slider.js
+++ src/slider.js
@@ -1,4 +1,4 @@
-import {div, input, p, span} from '@cycle/dom'
+import {br, div, input, p, span} from '@cycle/dom'
 
 
 export const Slider = (sources, props) => {
@@ -7,27 +7,30 @@ export const Slider = (sources, props) => {
     const eq = (x) => x
     const print = props.print || eq
 
-  const new_value$ = DOM
-    .select('input')
-    .events('input')
-    .map(ev => conv(ev.target.value))
-    .startWith(props.value)
+    const new_value$ = DOM
+	  .select('input')
+	  .events('input')
+	  .map(ev => conv(ev.target.value))
+	  .startWith(props.value)
 
-  const vdom$ = new_value$
-    .map(state =>
-      div(props.className, [
-	span('.label', [props.label]),
-	input({attrs: {className: '.slider', type:'range',
-		       style: 'width: 500px', step: props.step,
-		       min: props.min, max: props.max, value: state}}),
-	  span([print(state)]),
-      ])
-    )
+    const vdom$ = new_value$
+	  .map(state =>
+	       div({attrs: {
+		   style: 'text-align: center',
+		   className: props.className}}, [
+		   span('.label', [props.label]),
+		   br(),
+		   input({attrs: {className: '.slider', type:'range',
+				  style: 'width: 500px', step: props.step,
+				  min: props.min, max: props.max, value: state}}),
+		   span([print(state)]),
+	       ])
+	      )
 
-  const sinks = {
-    DOM: vdom$,
-    value: new_value$
-  }
+    const sinks = {
+	DOM: vdom$,
+	value: new_value$
+    }
 
-  return sinks
+    return sinks
 }
