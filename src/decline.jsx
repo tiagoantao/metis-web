62381d0a590a613c19186267bad2d2b4f8bb440f
Tiago Antao <tiagoantao@gmail.com>
2018-02-19 18:18:09 -0700

    ExpHz on table

--- src/decline.jsx
+++ src/decline.jsx
@@ -3,6 +3,7 @@ import Rx from 'rxjs/Rx'
 import {Plot} from './plot.js'
 import {Selector} from './selector.js'
 import {Slider} from './slider.js'
+import {Table} from './table.js'
 
 
 import {
@@ -24,7 +25,6 @@ import {
   sp_Species} from '@tiagoantao/metis-sim'
 
 
-
 class SizeChange {  //XXX Very ugly hack
   constructor(decline_cycle, end_pop_size) {
     this._decline_cycle = decline_cycle
@@ -40,10 +40,10 @@ class SizeChange {  //XXX Very ugly hack
 
 
 const prepare_sim_state = (tag, start_pop_size, end_pop_size,
-			   decline_cycle, num_markers, marker_type) => {
-			     const genome_size = num_markers
+                           decline_cycle, num_markers, marker_type) => {
+                             const genome_size = num_markers
 
-			     console.log(marker_type)
+                             console.log(marker_type)
   const unlinked_genome = gn_generate_unlinked_genome(
     genome_size, () => {return marker_type === 'SNP'? new gn_SNP() : new gn_MicroSatellite(Array.from(new Array(10), (x,i) => i))})
   const species = new sp_Species('unlinked', unlinked_genome)
@@ -94,24 +94,24 @@ export const DeclineApp = (sources) => {
   marker_type_c.value.subscribe(v => marker_type = v)
   
   const start_pop_size_c = Slider({DOM: sources.DOM},
-                            {className: '.' + tag + '-start_pop_size', label: 'Starting population size',
-                             step: 10, min: 10, value: 200, max: 300})
+                                  {className: '.' + tag + '-start_pop_size', label: 'Starting population size',
+                                   step: 10, min: 10, value: 200, max: 300})
   let start_pop_size
   start_pop_size_c.value.subscribe(v => start_pop_size = v)
 
   const end_pop_size_c = Slider({DOM: sources.DOM},
-                            {className: '.' + tag + '-end_pop_size', label: 'Size after decline',
-                             step: 10, min: 10, value: 30, max: 300})
+                                {className: '.' + tag + '-end_pop_size', label: 'Size after decline',
+                                 step: 10, min: 10, value: 30, max: 300})
   let end_pop_size
   end_pop_size_c.value.subscribe(v => end_pop_size = v)
 
   const decline_cycle_c = Slider({DOM: sources.DOM},
-                            {className: '.' + tag + '-decline_cycle', label: 'Decline cycle',
-                             step: 10, min: 10, value: 100, max: 200})
+                                 {className: '.' + tag + '-decline_cycle', label: 'Decline cycle',
+                                  step: 10, min: 10, value: 100, max: 200})
   let decline_cycle
   decline_cycle_c.value.subscribe(v => decline_cycle = v)
 
-    
+  
   const num_cycles_c = Slider({DOM: sources.DOM},
                               {className: '.' + tag + '-num_cycles', label: 'Cycles',
                                step: 10, min: 10, value: 200, max: 500})
@@ -123,6 +123,16 @@ export const DeclineApp = (sources) => {
                                 step: 1, min: 1, value: 4, max: 20})
   let num_markers
   num_markers_c.value.subscribe(v => num_markers = v)
+
+  const exphe_clean$ = exphe$.map(arr => arr.map(v => {
+    return {marker: v.marker, exp_he: Math.round(v.y * 100) / 100}}))
+
+  const exphe_table = Table(
+    {DOM: sources.DOM,
+     data: exphe_clean$.startWith([])},
+    {fields: ['marker', 'exp_he'],
+     headers: ['Marker', 'Expected Hz']}
+  )
 
   const exphe_plot = Plot(
     {id: tag + '-exphe', y_label: 'Expected Heterozygosity'},
@@ -140,31 +150,34 @@ export const DeclineApp = (sources) => {
     const init = {
       num_cycles,
       state: prepare_sim_state(tag, start_pop_size, end_pop_size,
-			       decline_cycle, num_markers, marker_type)
+                               decline_cycle, num_markers, marker_type)
     }
     return init
   })
 
-  const vdom$ = Rx.Observable
-        .combineLatest(
-            marker_type_c.DOM, start_pop_size_c.DOM,
-	    end_pop_size_c.DOM, decline_cycle_c.DOM,
-	    num_cycles_c.DOM, num_markers_c.DOM,
-            exphe_plot.DOM, numal_plot.DOM)
+  const vdom$ = Rx.Observable.combineLatest(
+    marker_type_c.DOM, start_pop_size_c.DOM,
+    end_pop_size_c.DOM, decline_cycle_c.DOM,
+    num_cycles_c.DOM, num_markers_c.DOM,
+    exphe_table.DOM,
+    exphe_plot.DOM, numal_plot.DOM)
         .map(([marker_type, start_pop_size, end_pop_size, decline_cycle,
-	       num_cycles, num_markers,
+               num_cycles, num_markers,
+               exphe_table,
                exphe, numal]) =>
                     <div>
                       <div>
                         {marker_type}
                         {start_pop_size}
-	                {end_pop_size}
+                        {end_pop_size}
                         {decline_cycle}
                         {num_cycles}
                         {num_markers}
                         <br/>
                         <button id={tag} value="1">Simulate</button>
                       </div>
+                      <h2>Expected Heterozygosity</h2>
+                      {exphe_table}
                       {exphe}
                       {numal}
                     </div>
